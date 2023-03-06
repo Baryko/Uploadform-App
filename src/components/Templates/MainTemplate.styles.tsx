a246da20508b093ff23d466fa62eec5482ddfc0f
Bartosz Korolik <korolik.bartosz@gmail.com>
2023-03-06 20:13:13 +0100

    add styles to MainTemplate

---
+++ src/components/Templates/MainTemplate.styles.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+import backgroundImage from '../../../assets/images/background.jpg';
+
+export const MainTemplate = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  grid-template-columns: repeat(12, minmax(150px, 1fr));
+  grid-template-rows: repeat(12, minmax(50px, 1fr));
+  overflow: hidden;
+`;
