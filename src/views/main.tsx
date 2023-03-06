b7475e6053220274fa9e247a70fadf80133906de
Bartosz Korolik <korolik.bartosz@gmail.com>
2023-03-06 21:04:20 +0100

    add GlobalStyles and modify main.tsx with it

--- src/views/main.tsx
+++ src/views/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyle } from '../assets/styles/GlobalStyles/GlobalStyles';
+
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
