053b40dfaa1bfeafe39035ed1710ecb8749a36c6
Bartosz Korolik <korolik.bartosz@gmail.com>
2023-03-11 19:29:19 +0100

    create HandleClearForm logic to reset form and set On-click on reset-button

--- src/components/Atoms/ResetButton/ResetButton.tsx
+++ src/components/Atoms/ResetButton/ResetButton.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Button from './ResetButton.styles';
+import { Props } from './ResetButton.types';
 
-function ResetButton() {
-  return <Button type="button">Start again</Button>;
+function ResetButton({ handleClearForm }: Props) {
+  return (
+    <Button type="button" onClick={handleClearForm}>
+      Start again
+    </Button>
+  );
 }
 
 export default ResetButton;
