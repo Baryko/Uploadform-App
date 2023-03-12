9054b3fa11360af4114640d4438ccde9a99ec9e8
Bartosz Korolik <korolik.bartosz@gmail.com>
2023-03-12 17:33:31 +0100

    adjust form media-queries

--- src/components/Molecules/FilesUpload/FilesUpload.styles.tsx
+++ src/components/Molecules/FilesUpload/FilesUpload.styles.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
+import Label from '../../Atoms/Label/Label';
+
 export const UploadField = styled.div`
   border: 0px dashed black;
   background-color: #846d9c36;
   margin-bottom: 0px;
   position: relative;
   border-radius: 15px;
+
   @media screen and (min-width: 1280px) {
+    height: 55px;
+  }
+  @media screen and (min-width: 1536px) {
     height: 75px;
   }
   @media screen and (min-width: 1920px) {
@@ -16,10 +22,15 @@ export const UploadField = styled.div`
   &.dragover {
     opacity: 0.6;
   }
+
   SVG {
     position: absolute;
     top: 40px;
     @media screen and (min-width: 1280px) {
+      font-size: 16px;
+      top: 20px;
+    }
+    @media screen and (min-width: 1536px) {
       font-size: 18px;
       top: 30px;
     }
